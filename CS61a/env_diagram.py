4d92bd9944f62236c46f03a63cfe16ae94112390
Titas Dutta <duttat@uci.edu>
2019-07-29 11:23:50 -0700

    first blank commit

---
+++ CS61a/env_diagram.py
@@ -0,0 +1,10 @@
+def square(real_num):
+    """The function squares the input."""
+    return real_num * real_num
+
+
+def sum_of_squares(x, y):
+    return square(x) + square(y)
+
+
+sum_of_squares(3, 5)
